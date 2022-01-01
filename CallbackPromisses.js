7e127b8d5ea2931580de08b3e8fa9b422dca3e87
AndMe <andrevieira@lme.ufc.br>
2022-01-01 16:38:19 -0300

    Exewmplos de função

--- CallbackPromisses.js
+++ CallbackPromisses.js
@@ -11,3 +11,20 @@ function doOtherThing (callback) {
 		callback('Second data')
 	}, 1000)
 }
+
+
+function doAll() {
+	doSomething(function(data) {
+		var processedData = data.split('');
+
+		doOtherThing(function(data2) {
+			var processedData2 = data2.split('');
+
+			setTimeout(function() {
+			console.log(processedData, processedData2)
+			}, 1000)
+		})
+	})
+}
+
+doAll()
