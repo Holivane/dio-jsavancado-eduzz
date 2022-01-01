a977ca9a6dbae0d60c7fd02dd8d7dd6109a75aae
AndMe <andrevieira@lme.ufc.br>
2022-01-01 17:38:01 -0300

    Tratamento de possíveis erros

--- CallbackPromisses.js
+++ CallbackPromisses.js
@@ -1,12 +1,12 @@
-function doSomething (callback) {
-	setTimeout(function(){
+function doSomething(callback) {
+	setTimeout(function () {
 		//did something
 		callback('First data')
 	}, 1000)
 }
 
-function doOtherThing (callback) {
-	setTimeout(function(){
+function doOtherThing(callback) {
+	setTimeout(function () {
 		//did other thing
 		callback('Second data')
 	}, 1000)
@@ -14,17 +14,25 @@ function doOtherThing (callback) {
 
 
 function doAll() {
-	doSomething(function(data) {
-		var processedData = data.split('');
+	try {
+		doSomething(function (data) {
+			var processedData = data.split('');
 
-		doOtherThing(function(data2) {
-			var processedData2 = data2.split('');
+			try {
+				doOtherThing(function (data2) {
+					var processedData2 = data2.split('');
 
-			setTimeout(function() {
-			console.log(processedData, processedData2)
-			}, 1000)
+					setTimeout(function () {
+						console.log(processedData, processedData2)
+					}, 1000)
+				})
+			} catch (err) {
+				//handle erro
+			}
 		})
-	})
+	} catch (err) {
+		//handle error
+	}
 }
 
 doAll()
