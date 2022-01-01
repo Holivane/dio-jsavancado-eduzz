3a4d25d3581ea5abfefe064586fb67df0044c08d
AndMe <andrevieira@lme.ufc.br>
2022-01-01 16:22:08 -0300

    Dois exemplos de função

--- CallbackPromisses.js
+++ CallbackPromisses.js
@@ -4,3 +4,10 @@ function doSomething (callback) {
 		callback('First data')
 	}, 1000)
 }
+
+function doOtherThing (callback) {
+	setTimeout(function(){
+		//did other thing
+		callback('Second data')
+	}, 1000)
+}
